0130964acb5da3e1895eef0f9473eb5459d246d6
MedadRufus <mailto@medadnewman.co.uk>
2020-12-11 13:39:37 +0000

    dont send me an email unless there is a problem with the logger

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "running cron file"
-if ! pgrep -f MQTT_logger.py
+#echo "running cron file"
+if ! pgrep -f MQTT_logger.py > /dev/null
 then
-    echo "not running. firing up the logger"
+    echo "Mqtt Logger not running. firing up the logger"
     source /home/medadnew/virtualenv/HAB/3.6/bin/activate && cd /home/medadnew/HAB && nohup python MQTT_logger/MQTT_logger.py &
-else
-    echo "running"
+#else
+    #echo "running"
 fi
 
