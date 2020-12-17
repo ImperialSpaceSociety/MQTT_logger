baeee9e105f7b4d0434d108e314d37f429c95790
MedadRufus <mailto@medadnewman.co.uk>
2020-12-17 02:31:37 +0000

    put in tests to handle junk packets

--- test/test_packet_parser.py
+++ test/test_packet_parser.py
@@ -9,6 +9,7 @@ class Test_Scorer(TestCase):
         super(Test_Scorer, self).__init__(*args, **kwargs)
 
         self.raw_mqtt_packet = '{"app_id":"icss_lora_tracker","dev_id":"icspace23","hardware_serial":"0093BECA9134091B","port":99,"counter":41,"payload_raw":"a03IKRX5Hs4ARgH6HtEA4wBCYQf6HtEACQFmYQf6HtEAhgIRfQf6HtEA6QAErgf6HtEA4wBCYQf6HtEACQFmYQf6HtEAhgIRfQf6HtEA6QAErgf6HtEA4wBCYQf6HtEACQFmYQf6HtEAhgIRfQf6HtEA6QAErgc=","metadata":{"time":"2020-12-15T13:32:30.644741305Z","frequency":868.1,"modulation":"LORA","data_rate":"SF8BW125","airtime":389632000,"coding_rate":"4/5","gateways":[{"gtw_id":"eui-0000024b0b03046b","timestamp":2834815476,"time":"2020-12-15T13:32:30.62244Z","channel":0,"rssi":-66,"snr":11.2,"rf_chain":0,"latitude":51.96731,"longitude":1.35357,"altitude":32}]}}'
+        self.junk_mqtt_packet = '{"app_id":"icss_lora_tracker","dev_id":"icspace23","hardware_serial":"0093BECA9134091B","port":0,"counter":39,"payload_raw":null,"metadata":{"time":"2020-12-15T13:31:02.338677749Z","frequency":868.5,"modulation":"LORA","data_rate":"SF8BW125","airtime":82432000,"coding_rate":"4/5","gateways":[{"gtw_id":"eui-0000024b0b03046b","timestamp":2746508244,"time":"2020-12-15T13:31:02.315183Z","channel":2,"rssi":-67,"snr":9,"rf_chain":0,"latitude":51.96731,"longitude":1.35355,"altitude":32}]}}'
 
     def test_if_current_long_is_parsed_correctly(self):
         pac = PacketParser(raw_packet=self.raw_mqtt_packet)
@@ -28,6 +29,6 @@ class Test_Scorer(TestCase):
 
         self.assertAlmostEqual(target_time, pac.current_time, delta=timedelta(seconds=1))
 
-
-if __name__ == '__main__':
-    unittest.main()
+    def test_if_can_parse_junk_packet(self):
+        pac = PacketParser(raw_packet=self.junk_mqtt_packet)
+        self.assertEqual(False, pac.parse_success)
