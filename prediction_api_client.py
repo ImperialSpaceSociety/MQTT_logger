212448a0a6a29cbd07b0dc6cac69ccfed7cd3c9f
MedadRufus <mailto@medadnewman.co.uk>
2020-12-17 00:54:23 +0000

    Update prediction_api_client.py

--- prediction_api_client.py
+++ prediction_api_client.py
@@ -1,5 +1,6 @@
-from requests import Session, Request
-from datetime import datetime, timezone, timedelta
+from datetime import datetime, timedelta
+
+import requests
 
 BASE_URL = "http://predict.cusf.co.uk/api/v1/"
 
@@ -11,25 +12,16 @@ class PredictApiClient:
     def __init__(self):
         pass
 
-    def make_request(self,start_time,end_time):
-
-
+    def make_request(self, start_time: datetime, hours_ahead: int, current_alt, current_long, current_lat):
         params = {
-            "launch_latitude": 52,
-            "launch_longitude": 0,
-            "launch_altitude": 0,
-            "launch_datetime": start_time,
+            "launch_latitude": current_lat,
+            "launch_longitude": current_long,
+            "launch_altitude": current_alt,
+            "launch_datetime": start_time.astimezone().isoformat(),
             "ascent_rate": 0.8,
-            "float_altitude": 12000,
-            "stop_datetime": end_time,
+            "float_altitude": current_alt,
+            "stop_datetime": (start_time + timedelta(hours=hours_ahead)).astimezone().isoformat(),
             "profile": "float_profile"
 
         }
-
-        s = Session()
-        p = Request('GET', BASE_URL, params=params).prepare()
-        r = s.send(p)
-
-        #print(r.content)
-
-        return p.url
+        return requests.get(BASE_URL, params=params)
