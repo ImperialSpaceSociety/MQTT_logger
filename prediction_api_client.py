6a3e1d46280fa1fb42c74b6f9558007078223499
MedadRufus <mailto@medadnewman.co.uk>
2020-12-17 02:22:24 +0000

    ensure start altitude is lower than float altitude for api

--- prediction_api_client.py
+++ prediction_api_client.py
@@ -5,7 +5,6 @@ import requests
 BASE_URL = "http://predict.cusf.co.uk/api/v1/"
 
 
-# http://predict.cusf.co.uk/api/v1/?launch_latitude=52&launch_longitude=0&launch_altitude=0&launch_datetime=2020-12-09T11%3A30%3A00%2B00:00&ascent_rate=0.8&float_altitude=12000&stop_datetime=2020-12-15T11%3A30%3A00%2B00:00&profile=float_profile
 
 
 class PredictApiClient:
@@ -16,7 +15,7 @@ class PredictApiClient:
         params = {
             "launch_latitude": current_lat,
             "launch_longitude": current_long,
-            "launch_altitude": current_alt,
+            "launch_altitude": current_alt-1,
             "launch_datetime": start_time.astimezone().isoformat(),
             "ascent_rate": 0.8,
             "float_altitude": current_alt,
