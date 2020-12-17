f4fa62e3c8a2d3afbcd5f70086659be879db2a3b
MedadRufus <mailto@medadnewman.co.uk>
2020-12-17 02:32:06 +0000

    silently pass if None packets arrive

--- MQTT_logger.py
+++ MQTT_logger.py
@@ -113,6 +113,10 @@ class ThreadedMQTTLogger(Thread):
         """
         # parse packet
         parsed_pkt = PacketParser(incoming_pkt) # TODO: figure out how to fake the parsed packet with current time.
+
+        if not parsed_pkt.parse_success:
+            return
+
         # request prediction of flight
         prediction = self.predictapiclient.make_request(parsed_pkt.current_time,
                                                         180,
--- packet_parser.py
+++ packet_parser.py
@@ -11,6 +11,7 @@ class PacketParser:
         self.current_time = 0
 
         self.parse_packet()
+        self.parse_success = False
 
     def parse_packet(self):
         payload_json = json.loads(self.raw_packet)
@@ -20,8 +21,16 @@ class PacketParser:
 
 
         raw_payload = payload_json["payload_raw"]
+
+        if raw_payload == None:
+            self.parse_success = False
+            return
+
+
         hex_payload = base64.b64decode(raw_payload)
 
         self.current_long = int.from_bytes(hex_payload[7:9], byteorder="little", signed=True) * 0xffff / 1e7
         self.current_lat = int.from_bytes(hex_payload[5:7], byteorder="little", signed=True) * 0xffff / 1e7
         self.current_alt = int(int.from_bytes(hex_payload[9:11], byteorder="little", signed=False) * 0xff / 1000)
+
+        self.parse_success = True
