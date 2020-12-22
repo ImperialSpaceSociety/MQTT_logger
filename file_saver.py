130caaccbf80f31230e254bead6d1d4463c4686f
MedadRufus <mailto@medadnewman.co.uk>
2020-12-22 17:48:16 +0000

    use global for data dump location

--- file_saver.py
+++ file_saver.py
@@ -1,12 +1,14 @@
 from pathlib import Path
 
+data_dump_location = Path("datadump/")
+
+
 class FileSaver:
     def __init__(self):
         pass
 
     @staticmethod
-    def save_file(filename: str, content : bytes):
-        p = Path("datadump/")
-        p.mkdir(parents=True, exist_ok=True)
-        filepath = p / filename
+    def save_file(filename: str, content: bytes):
+        data_dump_location.mkdir(parents=True, exist_ok=True)
+        filepath = data_dump_location / filename
         filepath.write_bytes(content)
