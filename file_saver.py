7869d931209e12df22ba786f99fadd8ce41a2b7b
MedadRufus <mailto@medadnewman.co.uk>
2020-12-22 22:45:11 +0000

    Update file_saver.py

--- file_saver.py
+++ file_saver.py
@@ -1,14 +1,14 @@
 from pathlib import Path
 
 data_dump_location = Path("datadump/")
-
+html_render_location = Path("renders/")
 
 class FileSaver:
     def __init__(self):
-        pass
+        data_dump_location.mkdir(parents=True, exist_ok=True)
+        html_render_location.mkdir(parents=True, exist_ok=True)
 
     @staticmethod
     def save_file(filename: str, content: bytes):
-        data_dump_location.mkdir(parents=True, exist_ok=True)
         filepath = data_dump_location / filename
         filepath.write_bytes(content)
