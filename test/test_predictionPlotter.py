8f35b37872be877f247d90667df69f2e3261a25e
MedadRufus <mailto@medadnewman.co.uk>
2020-12-23 18:11:20 +0000

    put in a test to plot the prediction

--- test/test_predictionPlotter.py
+++ test/test_predictionPlotter.py
@@ -10,5 +10,10 @@ class TestPredictionPlotter(TestCase):
         self.pp = PredictionPlotter()
 
     def test_plot_and_save(self):
-        json_fp = Path("datadump/forward_prediction_at_2020-12-22_16-00-02.json")
+        json_fp = Path("datadump/forward_prediction_at_2020-12-23_16-00-01.json")
         self.pp.plot_and_save(json_fp)
+
+    def test_plot_and_show(self):
+        json_fp = Path("datadump/forward_prediction_at_2020-12-23_16-00-01.json")
+        p = self.pp.plot(json_fp)
+        self.pp.show_plot(p)
