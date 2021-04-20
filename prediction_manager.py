a2d0fdc6e699c0ecdd234d6d047dd99f89082bf3
MedadRufus <medadrufus@gmail.com>
2021-04-20 22:50:58 +0100

    update new input requirements for the api

--- prediction_manager.py
+++ prediction_manager.py
@@ -27,9 +27,9 @@ class PredictionManager:
     def make_request(self, start_time: datetime, hours_ahead: int, current_alt, current_long, current_lat):
         params = {
             "launch_latitude": current_lat,
-            "launch_longitude": current_long,
+            "launch_longitude": current_long % 360,  # convert from [-180,180] to [0,360]
             "launch_altitude": current_alt - 1,
-            "launch_datetime": start_time.astimezone().isoformat(),
+            "launch_datetime": start_time.astimezone().isoformat() + "Z",
             "ascent_rate": 0.8,
             "float_altitude": current_alt,
             "stop_datetime": (start_time + timedelta(hours=hours_ahead)).astimezone().isoformat(),
