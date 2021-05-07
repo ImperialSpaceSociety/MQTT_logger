1d5496b9cedfc8fb5f5abda77c06bd293ba75650
MedadRufus <medadrufus@gmail.com>
2021-05-07 02:25:14 +0100

    fix failing test

--- test/test_packet_parser.py
+++ test/test_packet_parser.py
@@ -9,9 +9,13 @@ class Test_Scorer(TestCase):
         self.raw_mqtt_packet = '{"app_id":"icss_lora_tracker","dev_id":"icspace23","hardware_serial":"0093BECA9134091B","port":99,"counter":41,"payload_raw":"a03IKRX5Hs4ARgH6HtEA4wBCYQf6HtEACQFmYQf6HtEAhgIRfQf6HtEA6QAErgf6HtEA4wBCYQf6HtEACQFmYQf6HtEAhgIRfQf6HtEA6QAErgf6HtEA4wBCYQf6HtEACQFmYQf6HtEAhgIRfQf6HtEA6QAErgc=","metadata":{"time":"2020-12-15T13:32:30.644741305Z","frequency":868.1,"modulation":"LORA","data_rate":"SF8BW125","airtime":389632000,"coding_rate":"4/5","gateways":[{"gtw_id":"eui-0000024b0b03046b","timestamp":2834815476,"time":"2020-12-15T13:32:30.62244Z","channel":0,"rssi":-66,"snr":11.2,"rf_chain":0,"latitude":51.96731,"longitude":1.35357,"altitude":32}]}}'
         self.junk_mqtt_packet = '{"app_id":"icss_lora_tracker","dev_id":"icspace23","hardware_serial":"0093BECA9134091B","port":0,"counter":39,"payload_raw":null,"metadata":{"time":"2020-12-15T13:31:02.338677749Z","frequency":868.5,"modulation":"LORA","data_rate":"SF8BW125","airtime":82432000,"coding_rate":"4/5","gateways":[{"gtw_id":"eui-0000024b0b03046b","timestamp":2746508244,"time":"2020-12-15T13:31:02.315183Z","channel":2,"rssi":-67,"snr":9,"rf_chain":0,"latitude":51.96731,"longitude":1.35355,"altitude":32}]}}'
         self.test_raw_payload = "a0HKQBS3HuD/pP+3HuD/OQHkBbce4P8+Ae4Ftx7g/1QB6AW3HuD/SgHyBbce4P86AewFtx7g/zoB8AW3HuD/ZQHiBbce4P9NAeYFtx7g/yEBHgC3HuD/PgHqBbce4P85AeQFtx7g/z4B7gW3HuD/VAHoBQ=="
+
+        self.current_time = datetime.strptime("2021-05-07T01:03:52.500139", '%Y-%m-%dT%H:%M:%S.%f')
+        self.current_time = self.current_time.replace(tzinfo=timezone.utc)
+
     def test_parse_raw_packet(self):
         pac = PacketParser(raw_packet=self.raw_mqtt_packet)
-        pac.parse_raw_payload(self.test_raw_payload)
+        pac.parse_raw_payload(self.test_raw_payload, self.current_time)
 
         self.assertEqual(16688, pac.current_alt)
         self.assertEqual(8, pac.num_sats)
