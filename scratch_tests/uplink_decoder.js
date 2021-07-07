f2ce14bc0df2e753c4fc1910718bed56151fc1bd
MedadRufus <medadrufus@gmail.com>
2021-07-07 23:58:19 +0100

    arrange function

--- scratch_tests/uplink_decoder.js
+++ scratch_tests/uplink_decoder.js
@@ -1,14 +1,6 @@
 var units = ['C', 'Pa', 'kPa', 'MPa'];
 
-function bytesToFloat(bytes) {
-    "use strict";
-    var bits = bytes[3] << 24 | bytes[2] << 16 | bytes[1] << 8 | bytes[0];
-    var sign = (bits >>> 31 === 0) ? 1.0 : -1.0;
-    var e = bits >>> 23 & 0xff;
-    var m = (e === 0) ? (bits & 0x7fffff) << 1 : (bits & 0x7fffff) | 0x800000;
-    var f = sign * m * Math.pow(2, e - 150);
-    return f;
-}
+
 
 function decodeUplink(input) {
     "use strict";
@@ -40,4 +32,14 @@ function readUInt16LE(bytes) {
 function readInt16LE(bytes) {
     var ref = readUInt16LE(bytes);
     return ref > 0x7fff ? ref - 0x10000 : ref;
+}
+
+function bytesToFloat(bytes) {
+    "use strict";
+    var bits = bytes[3] << 24 | bytes[2] << 16 | bytes[1] << 8 | bytes[0];
+    var sign = (bits >>> 31 === 0) ? 1.0 : -1.0;
+    var e = bits >>> 23 & 0xff;
+    var m = (e === 0) ? (bits & 0x7fffff) << 1 : (bits & 0x7fffff) | 0x800000;
+    var f = sign * m * Math.pow(2, e - 150);
+    return f;
 }
